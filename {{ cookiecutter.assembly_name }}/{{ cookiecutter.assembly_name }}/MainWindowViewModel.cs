82235de6dc2c4c421a24235b1f2bc192613aaa77
Jonathan Arciaga <jonarciaga@gmail.com>
2020-03-24 12:51:14 -0500

    Initial Commit

---
+++ {{ cookiecutter.assembly_name }}/{{ cookiecutter.assembly_name }}/MainWindowViewModel.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using {{ cookiecutter.assembly_name }}.Models;
+
+namespace {{ cookiecutter.assembly_name }}
+{
+    public class MainWindowViewModel : INotifyPropertyChanged
+    {
+        #region " Constructor "
+
+        public MainWindowViewModel()
+        {
+			
+        }
+
+        #endregion
+
+        #region " Properties "
+
+        #endregion
+
+        #region " Methods "
+
+        #endregion
+
+        #region " INotifyPropertyChanged "
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged(string name)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+
+        #endregion
+    }
+}
